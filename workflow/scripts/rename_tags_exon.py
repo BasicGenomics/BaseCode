52933d0c661cfc9dcc5fcd0ba1e9a04ad396981f
AntonJMLarsson <antonlarsson23@gmail.com>
2025-05-30 16:25:18 +0200

    print debug

--- workflow/scripts/rename_tags_exon.py
+++ workflow/scripts/rename_tags_exon.py
@@ -19,7 +19,8 @@ def main(arguments):
         if read.has_tag('XT'):
             XT = read.get_tag('XT')
             read.set_tag('GE', XT)
-        tags = read.get_tags()
+        tags = read.get_tags(with_value_type=True)
+        print(tags[0][0], type(tags[0][0]))
         tags = [t for t in tags if t[0] not in ['XS', 'XT', 'XN']]
         read.set_tags(tags)
         pstrand_out.write(read)
@@ -32,7 +33,7 @@ def main(arguments):
         if read.has_tag('XT'):
             XT = read.get_tag('XT')
             read.set_tag('GE', XT)
-        tags = read.get_tags()
+        tags = read.get_tags(with_value_type=True)
         tags = [t for t in tags if t[0] not in ['XS', 'XT', 'XN']]
         read.set_tags(tags)
         mstrand_out.write(read)
@@ -45,7 +46,7 @@ def main(arguments):
         if read.has_tag('XT'):
             XT = read.get_tag('XT')
             read.set_tag('GE', XT)
-        tags = read.get_tags()
+        tags = read.get_tags(with_value_type=True)
         tags = [t for t in tags if t[0] not in ['XS', 'XT', 'XN']]
         read.set_tags(tags)
         nostrand_out.write(read)
